c62c2cb193f08c4d28d0e11f6ad1610d4ac0ca51
andy <andy.development@protonmail.com>
2022-01-15 02:07:16 -0500

    chore: Add types to parameter

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,11 +8,11 @@ async def test_on() -> None:
     client = rin.GatewayClient("DISCORD_TOKEN")
 
     @client.on(rin.Event.MESSAGE_CREATE)
-    async def message_create(_) -> None:
+    async def message_create(_: dict[str, str]) -> None:
         ...
 
     @client.once(rin.Event.READY)
-    async def ready(_) -> None:
+    async def ready(_: dict[str, str]) -> None:
         ...
 
     assert len(client.dispatch.listeners["message_create"]) == 1
