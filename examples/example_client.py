2fa545b9acd72101530e3e8bdafdd0ff60d08c3e
an-dyy <andy.development@protonmail.com>
2022-01-20 21:13:29 -0500

    types: Fix `example_client.py`

--- examples/example_client.py
+++ examples/example_client.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import asyncio
 import os
-from typing import Any
 
 import rin
 
@@ -10,15 +9,11 @@ import rin
 async def main() -> None:
     # pyright: reportUnusedFunction=false
     token: str = os.getenv("DISCORD_TOKEN")  # type: ignore
-    client = rin.GatewayClient(token, intents=32509)
+    client = rin.GatewayClient(token, intents=rin.Intents.default())
 
     @client.once(rin.Event.READY)
     async def on_ready(user: rin.User) -> None:
         print(f"Logged in as: {user.id}")
-
-    @client.on(rin.Event.MESSAGE_CREATE)
-    async def on_message(data: dict[Any, Any]) -> None:
-        print(data)
 
     await client.start()
 
