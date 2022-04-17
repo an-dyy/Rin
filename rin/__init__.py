8f3aae9ae4e547e612af325f519d178f224153e6
andy <andy.development@protonmail.com>
2022-04-16 23:52:41 -0400

    refactor!: Remove logger & add `loguru`

--- rin/__init__.py
+++ rin/__init__.py
@@ -4,8 +4,6 @@ __version__ = "0.2.0-alpha"
 
 from typing import Literal, NamedTuple
 
-from .logger import *
-
 
 class Version(NamedTuple):
     major: int
--- rin/__init__.pyi
+++ rin/__init__.pyi
@@ -1,6 +1,5 @@
 from rin.gateway import *
 from rin.interface import *
-from rin.logger import *
 from rin.rest import *
 
 __all__: list[str]
